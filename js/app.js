c4f7feaf7ab068dda18672bb1bc392d0d152cbf4
darren.beckwith <dbeckwith@carmanah.com>
2019-01-08 16:10:55 -0800

    feat: Add player class and render function
    Commented

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function(x, y) {
+    this.x = x,
+    this.y = y,
+
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -21,15 +22,36 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y) {
+    this.x = x,
+    this.y = y,
+
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(keyCode) {
+
+}
+
+
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
+var player = new Player(505/2 - 101/2, 400);
 
 
 // This listens for key presses and sends the keys to your
--- js/engine.js
+++ js/engine.js
@@ -78,7 +78,8 @@ var Engine = (function(global) {
      * on the entities themselves within your app.js file).
      */
     function update(dt) {
-        updateEntities(dt);
+        //TODO Add back to enable enemy functionality
+        // updateEntities(dt);
         // checkCollisions();
     }
 
@@ -149,9 +150,10 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
-        allEnemies.forEach(function(enemy) {
-            enemy.render();
-        });
+        //TODO Add back to enable enemy functionality
+        // allEnemies.forEach(function(enemy) {
+        //     enemy.render();
+        // });
 
         player.render();
     }
